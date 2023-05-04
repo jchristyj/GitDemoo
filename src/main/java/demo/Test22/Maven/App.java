8c1ebd1a020afb7e5879c7126092727aed3263bb
Ashok_Nagappan <jjancychristy1988@gmail.com>
2023-05-04 12:49:45 -0400

    asian y

--- src/main/java/demo/Test22/Maven/App.java
+++ src/main/java/demo/Test22/Maven/App.java
@@ -9,12 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );//code by X
-        System.out.println( "Hello World!" );//code by Y
-        System.out.println( "Hello World!" );//code by Xagain
-        
-        System.out.println( "Hello World!" );//code by Xagain
-        
-        System.out.println( "Hello World!" );//code by Xagain
+        System.out.println( "Hello World!" );//code corrected by Y
         
         
         
